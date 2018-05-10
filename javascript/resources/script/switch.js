aa5a26d1da6a46fa1b772b9e6b21135e2c0fea78
BishalRana <38860618+BishalRana@users.noreply.github.com>
2018-05-10 16:21:40 +0100

    Add files via upload

---
+++ javascript/resources/script/switch.js
@@ -0,0 +1,4 @@
+//create a  switch statement that displays the current day and time of the week on the webpage
+//time:hours/minutes/seconds
+//Day:specifiy the actual day
+//switch statements to be used in th e 
